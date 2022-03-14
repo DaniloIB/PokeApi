3fa5818b41357a60d1ce72b80359783283a4908c
Danilo Ibañez <65428303+DaniloIB@users.noreply.github.com>
2022-03-14 01:11:41 -0300

    PokeApi

---
+++ js/script.js
@@ -0,0 +1,54 @@
+const cards_container = document.getElementById("cards-container");
+const pokemon_count = 20;
+
+const fetchPokemons = async () => {
+  for (let i = 1; i <= pokemon_count; i++) {
+    await getPokemon(i);
+  }
+};
+
+const getPokemon = async (id) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data)
+  createPokemonCard(data);
+};
+
+const createPokemonCard = (pokemon) => {
+  
+  const pokemonEl = document.createElement("div");
+  pokemonEl.classList.add("card");
+
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+  const type = pokemon.types[0].type.name;
+  const hp = pokemon.stats[5].base_stat;
+  const weight = pokemon.weight;
+  const height = pokemon.height / 10;
+  
+
+
+  const pokemonInnerHTML = `
+    <div class="cover">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg" alt="">
+                <div class="img__back ${type}"></div>
+            </div>
+            <div class="description">
+                <h2 class="name">${name}</h2>
+                <div class="hp">
+                    HP ${hp}/${hp}
+                </div>
+                <ul class="stats">
+                    <li>${type}<br /><span>Type</span></li>
+                     <li>${weight}kg<br /><span>Weight</span></li>
+                    <li>${height}m<br /><span>Height</span></li>
+                </ul>  
+            </div>
+    `;
+
+  pokemonEl.innerHTML = pokemonInnerHTML;
+
+  cards_container.appendChild(pokemonEl);
+};
+
+fetchPokemons();
